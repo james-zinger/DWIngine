daac5634cc7c66c6414726fba893b2fe0e540681
Jeff Rose <jrose0@gmail.com>
2013-10-22 20:25:05 -0400

    Added DWMath with its glorious 10 lines of alising.

---
+++ DWIngine/DWIngine/DWMath.h
@@ -0,0 +1,10 @@
+#include <glm\core\type_mat4x4.hpp>
+#include <glm\core\type_vec3.hpp>
+#include <glm\gtc\quaternion.hpp>
+
+namespace dwi
+{
+	typedef glm::mat4 Matrix4;
+	typedef glm::vec3 Vector3;
+	typedef glm::quat Quaternion;
+}
